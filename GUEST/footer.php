28eeda0202beecb14b5a8a82ef0699ed82b341f1
minhtamphan373 <minhtamphan373@gmail.com>
2024-10-03 11:05:26 +0700

    thay đổi vị trí thư mục

---
+++ GUEST/footer.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- SCSS -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Footer</title>
+</head>
+
+<body>
+    <footer class="footer">
+        <div class="container">
+            <div class="row align-items-start">
+                <div class="col-6">
+                    <h6 class="footer-heading">trường mầm non hoa mai</h6>
+                    <ul class="list-inline">
+                        <li class="py-3 px-1"><a href="#">Địa chỉ: 793/57/6 Trần Xuân Soạn phường Tân Hưng Quận 7 Thành
+                                phố
+                                Hồ Chí Minh</a></li>
+                        <li class="py-3 px-1"><a href="#">Số điện thoại: 0123456789</a></li>
+                        <li class="py-3 px-1"><a href="#">Email: mamnonhoamaiquan7@gmail.com</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <h6 class="footer-heading">về chúng tôi</h6>
+                    <ul class="list-inline">
+                        <li class="py-3 px-1"><a href="#">Trang chủ</a></li>
+                        <li class="py-3 px-1"><a href="#">Chương trình học</a></li>
+                        <li class="py-3 px-1"><a href="#">Tin tức - Sự kiện</a></li>
+                        <li class="py-3 px-1"><a href="#">Tuyển sinh</a></li>
+                        <li class="py-3 px-1"><a href="#">Liên hệ</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h6 class="footer-heading">Map</h6>
+                </div>
+            </div>
+
+            <div class="w-100 border-top "></div>
+
+            <div class="row">
+                <p class="copyright">© Copyright 2024 by mamnonhoamaiquan7 | All rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ GUEST/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Header</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-sm">
+        <a class="navbar-brand" href="#">
+            <img src="../assets/img/Logo tách nền.png" alt="Avatar Logo" class="rounded-pill">
+        </a>
+        <div class="container ">
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item heading">
+                        <a class="nav-link" href="#">trang chủ</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle heading" href="#" role="button" data-bs-toggle="dropdown">tin
+                            tức - sự kiện</a>
+                        <ul class="dropdown-menu title">
+                            <li><a class="dropdown-item" href="#">Tin tức</a></li>
+                            <li><a class="dropdown-item" href="#">Sự kiện</a></li>
+                            <li><a class="dropdown-item" href="#">Văn bản</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle heading" href="#" role="button"
+                            data-bs-toggle="dropdown">chương trình học</a>
+                        <ul class="dropdown-menu title">
+                            <li><a class="dropdown-item" href="#">Chương trình mầm non</a></li>
+                            <li><a class="dropdown-item" href="#">Chương trình năng khiếu</a></li>
+                            <li><a class="dropdown-item" href="#">Giáo dục kỹ năng</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item heading">
+                        <a class="nav-link" href="#">tuyển sinh</a>
+                    </li>
+                    <li class="nav-item heading">
+                        <a class="nav-link" href="contact.php">liên hệ</a>
+                    </li>
+                </ul>
+
+                <button type="button" class="btnlogin text-sm-right" href="login.php">ĐĂNG NHẬP</button>
+
+            </div>
+
+        </div>
+    </nav>
+</body>
+
+</html>
